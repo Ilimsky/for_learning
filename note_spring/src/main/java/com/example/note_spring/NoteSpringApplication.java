5a7a30b0c3158183b052081d160ed235f6853605
Ilimsky <ilim.sarykbaev@gmail.com>
2023-01-27 08:55:14 +0300

    Created new app with Spring Boot and Android for learning developing of backend and Android and learing unit tests and integration test too

---
+++ note_spring/src/main/java/com/example/note_spring/NoteSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.note_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteSpringApplication.class, args);
+	}
+
+}
---
+++ note_spring/src/test/java/com/example/note_spring/NoteSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.note_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
